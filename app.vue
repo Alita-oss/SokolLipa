2d1a66508ec842f71178042c567c73fc895ba87a
Alita-oss <111301275+Alita-oss@users.noreply.github.com>
2024-08-18 15:10:29 +0200

    pwa

--- app.vue
+++ app.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="layout">
-    <SHeader />
-    <NuxtPage />
-    <SFooter />
-  </div>
-  <Transition name="fade">
-    <GamePopUp v-if="popupGame" :game="popupGame" @close="popupGame = null"/>
-  </Transition>
+    <div class="layout">
+        <SHeader />
+        <NuxtPwaManifest />
+        <NuxtPage />
+        <SFooter />
+    </div>
+    <Transition name="fade">
+        <GamePopUp v-if="popupGame" :game="popupGame" @close="popupGame = null" />
+    </Transition>
 </template>
 
 <script setup lang="ts">
-  import { useMainStore } from '~/stores/main';
+import { useMainStore } from '~/stores/main';
 
-  const { popupGame } = storeToRefs(useMainStore());
-
+const { popupGame } = storeToRefs(useMainStore());
 </script>
 
 <style scoped lang="scss">
 .layout {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding-top: 100px;
-  width: 100%;
-  min-height: 100vh;
-  min-height: 100dvh;
-  height: 100vh;
-  height: 100dvh;
-  position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 100px;
+    width: 100%;
+    min-height: 100vh;
+    min-height: 100dvh;
+    height: 100vh;
+    height: 100dvh;
+    position: relative;
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.2s ease;
+    transition: opacity 0.2s ease;
 }
 .fade-enter-from,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    css: ['~/assets/scss/main.scss'],
-    modules: [
-      '@nuxtjs/sanity',
-      '@pinia/nuxt',
-      '@nuxtjs/device',
-    ],
-    sanity: {
-      projectId: 'mgriwqg2'
-    }
+  devtools: { enabled: true },
+  css: ['~/assets/scss/main.scss'],
+
+  modules: ['@nuxtjs/sanity', '@pinia/nuxt', '@nuxtjs/device', "@vite-pwa/nuxt"],
+
+  sanity: {
+    projectId: 'mgriwqg2'
+  },
+
+  compatibilityDate: '2024-08-18'
 })
