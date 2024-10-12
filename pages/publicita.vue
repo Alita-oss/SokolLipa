4385502b2e116a6c9ba9fbe2a22053f8f84c6b4c
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2024-10-12 17:56:26 +0200

    fix(SL-2): Publicita image fix (#2)
    * fix: add cropped image
    
    * fix: rename images, delete old images

--- pages/publicita.vue
+++ pages/publicita.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="page">
-        <img src="~/assets/images/1.webp" alt="projekt Sokolovna Lípa">
-        <img src="~/assets/images/2.webp" alt="Program rozvoje venkova">
+        <img src="~/assets/images/projekt_sokolovna_lipa.webp" alt="projekt Sokolovna Lípa" />
+        <img src="~/assets/images/program_rozvoje_venkova.webp" alt="Program rozvoje venkova" />
     </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss">
 img {
