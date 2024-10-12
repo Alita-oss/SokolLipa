a7562cf3fedd8864370d8b83b87fb7c36a4e06bc
Alita-oss <111301275+Alita-oss@users.noreply.github.com>
2024-10-12 17:32:04 +0200

    fix: add cropped image

--- pages/publicita.vue
+++ pages/publicita.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="page">
-        <img src="~/assets/images/1.webp" alt="projekt Sokolovna Lípa">
-        <img src="~/assets/images/2.webp" alt="Program rozvoje venkova">
+        <img src="~/assets/images/1.webp" alt="projekt Sokolovna Lípa" />
+        <img src="~/assets/images/2-cropped.webp" alt="Program rozvoje venkova" />
     </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss">
 img {
